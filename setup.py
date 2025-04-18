2dba3d34e31ec73143ef80e9ef98ceddf3c87cde
Víctor Ortiz Gándara <gestordesarrollo@cartagena.gov.co>
2025-04-18 12:20:49 -0500

    Fix Setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,8 @@ setup(
     description='Tema Govco para CKAN',
     author='Víctor Ortiz Gándara',
     install_requires=[],
-    packages=['govcotheme'],
+    packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
+    namespace_packages=['ckanext'],
     include_package_data=True,
     zip_safe=False,
     entry_points='''
